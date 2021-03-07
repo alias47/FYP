60b7c0882713f04f17227269d48602c5abae7a83
alias47 <56735705+alias47@users.noreply.github.com>
2021-03-07 23:49:44 +0545

    first changes

---
+++ account/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+# Create your models here.
+
---
+++ account/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from account import views
+
+urlpatterns = [
+    path('',views.indexPage, name="home"),
+    path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
+    path('profile/', views.profilePage, name="profile"),
+    # path('profile/edit/', views.editProfilePage, name="editProfile"),
+    ]
